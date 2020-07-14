bb42263fbb500bbd0a7258c33d8f6ac0cf294fa8
zafar <zafar.parvez@gmail.com>
2020-07-14 15:16:18 +0530

    Created Seperated TopBar, SideBar, Footer, and Content.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+// import { SidebarComponent } from './ui-module/sidebar/sidebar.component';
+import { SidebarComponent } from './ui/sidebar/sidebar.component';
+import { TopbarComponent } from './ui/topbar/topbar.component';
+import { ContentComponent } from './ui/content/content.component';
+import { FooterComponent } from './ui/footer/footer.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    TopbarComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // SidebarComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
