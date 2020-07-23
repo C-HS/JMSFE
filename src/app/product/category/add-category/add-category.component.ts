443ec149bc4eb43ea650087e14a1981f4048b187
zafar <zafar.parvez@gmail.com>
2020-07-23 12:38:36 +0530

    Added the Category Add Form.

--- src/app/product/category/add-category/add-category.component.ts
+++ src/app/product/category/add-category/add-category.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-add-category',
@@ -6,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-category.component.css']
 })
 export class AddCategoryComponent implements OnInit {
-
-  constructor() { }
-
+  categoryForm: FormGroup;
+  categoryName: FormControl;
+  catelogName: FormControl;
+  constructor(private formBuilder: FormBuilder,
+              router: Router) {
+    this.createFromControls();
+    this.createForm();
+  }
+  createFromControls(): void{
+    this.categoryName = new FormControl('');
+    this.catelogName = new FormControl('');
+  }
+  createForm(): void{
+    this.categoryForm = this.formBuilder.group({
+      categoryName: this.categoryName,
+      catalogName: this.catelogName
+    });
+  }
   ngOnInit(): void {
   }
 
