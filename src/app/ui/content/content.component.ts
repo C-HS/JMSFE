11ce55acae2ffd28151492ad315492f2086f808f
zafar <zafar.parvez@gmail.com>
2020-07-25 13:14:48 +0530

    Added the card view in main content.

--- src/app/ui/content/content.component.ts
+++ src/app/ui/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-content',
@@ -6,8 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
+  header;
+  constructor(router: Router) {
+    // router.events.pipe(
+    //   filter(event => event instanceof NavigationEnd)
+    //   )
+    //   .subscribe(event => {
+    //     this.header = event.url;
+    //       console.log(event);
+    //   });
 
-  constructor() { }
+    router.events.subscribe(
+      (event: any) => {
+        if (event instanceof NavigationEnd) {
+          this.header = router.url;
+          console.log('this.router.url', router.url);
+        }
+      }
+    );
+  }
 
   ngOnInit(): void {
   }
